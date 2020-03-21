1a3f5279fb9c6b8785851d18ba1f5d17b56e2bea
chrisisnotarudeguy <christian.ricci2000@gmail.com>
2020-03-21 15:37:47 +0100

    Added an implementation of getline() for Windows. Might not be as performant as the real getline().

--- makepal.c
+++ makepal.c
@@ -22,6 +22,10 @@
 static mainprog_info maininfo;
 
 int readcolors(FILE *);
+#ifdef _WIN32
+typedef intptr_t ssize_t;
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+#endif
 
 int main(int argc, char **argv)
 {
@@ -168,4 +172,49 @@ int readcolors(FILE *infile)
 
     return 0;
 }
+
+#ifdef _WIN32
+ssize_t getline(char **lineptr, size_t *n, FILE *stream)
+{
+    size_t pos;
+    int c;
+    
+    if (lineptr == NULL || stream == NULL || n == NULL)
+        return -1;
+    
+    if ((c = getc(stream)) == EOF)
+        return -1;
+    
+    if (*lineptr == NULL) {
+        *lineptr = malloc(128);
+        if (*lineptr == NULL) {
+            return -1;
+        }
+        *n = 128;
+    }
+
+    pos = 0;
+    while(c != EOF) {
+        if (pos + 1 >= *n) {
+            size_t new_size = *n + (*n >> 2);
+            if (new_size < 128) {
+                new_size = 128;
+            }
+            char *new_ptr = realloc(*lineptr, new_size);
+            if (new_ptr == NULL)
+                return -1;
+            *n = new_size;
+            *lineptr = new_ptr;
+        }
+
+        (*lineptr)[pos++] = c;
+        if (c == '\n')
+            break;
+        c = getc(stream);
+    }
+
+    (*lineptr)[pos] = '\0';
+    return pos;
+}
+#endif
 
